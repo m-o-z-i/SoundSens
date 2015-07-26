2d4049df13d05a69777bb226884a0a420acab292
Moritz Loos <moritz.loos@uni-weimar.de>
2015-07-26 20:48:47 +0200

    increase tone length.

--- sounds.py
+++ sounds.py
@@ -4,17 +4,21 @@ from wavebender import *
 
 def precomputeSound():
     #precompute sound
-    soundlength = 0.4
+    soundlength = 0.8
     soundlength_L = 2
-    l1 = 44100 * 0.4
+    l1 = 44100 * soundlength
     l2 = l1
+
+    lengthFactor1 = 4
+    lengthFactor2 = 4
+
 
     toneC   = C(l1, l2)
     channelsC = ((toneC,), (toneC,))
     samplesC = compute_samples(channelsC, 44100 * soundlength)
     write_wavefile('tones/C.wav', samplesC, 44100 * soundlength, nchannels=2)
 
-    toneC_L   = C(12 * l1, 3 * l2)
+    toneC_L   = C(lengthFactor1 * l1, lengthFactor2 * l2)
     channelsC_L = ((toneC_L,), (toneC_L,))
     samplesC_L = compute_samples(channelsC_L, 44100 * soundlength_L)
     write_wavefile('tones/C_L.wav', samplesC_L, 44100 * soundlength_L, nchannels=2)
@@ -24,7 +28,7 @@ def precomputeSound():
     samplesD = compute_samples(channelsD, 44100 * soundlength)
     write_wavefile('tones/D.wav', samplesD, 44100 * soundlength, nchannels=2)
 
-    toneD_L   = D(12 * l1, 3 * l2)
+    toneD_L   = D(lengthFactor1 * l1, lengthFactor2 * l2)
     channelsD_L = ((toneD_L,), (toneD_L,))
     samplesD_L = compute_samples(channelsD_L, 44100 * soundlength_L)
     write_wavefile('tones/D_L.wav', samplesD_L, 44100 * soundlength_L, nchannels=2)
@@ -34,7 +38,7 @@ def precomputeSound():
     samplesE = compute_samples(channelsE, 44100 * soundlength)
     write_wavefile('tones/E.wav', samplesE, 44100 * soundlength, nchannels=2)
 
-    toneE_L   = E(12 * l1, 3 * l2)
+    toneE_L   = E(lengthFactor1 * l1, lengthFactor2 * l2)
     channelsE_L = ((toneE_L,), (toneE_L,))
     samplesE_L = compute_samples(channelsE_L, 44100 * soundlength_L)
     write_wavefile('tones/E_L.wav', samplesE_L, 44100 * soundlength_L, nchannels=2)
@@ -44,7 +48,7 @@ def precomputeSound():
     samplesF = compute_samples(channelsF, 44100 * soundlength)
     write_wavefile('tones/F.wav', samplesF, 44100 * soundlength, nchannels=2)
 
-    toneF_L   = F(12 * l1, 3 * l2)
+    toneF_L   = F(lengthFactor1 * l1, lengthFactor2 * l2)
     channelsF_L = ((toneF_L,), (toneF_L,))
     samplesF_L = compute_samples(channelsF_L, 44100 * soundlength_L)
     write_wavefile('tones/F_L.wav', samplesF_L, 44100 * soundlength_L, nchannels=2)
@@ -54,7 +58,7 @@ def precomputeSound():
     samplesG = compute_samples(channelsG, 44100 * soundlength)
     write_wavefile('tones/G.wav', samplesG, 44100 * soundlength, nchannels=2)
 
-    toneG_L   = G(12 * l1, 3 * l2)
+    toneG_L   = G(lengthFactor1 * l1, lengthFactor2 * l2)
     channelsG_L = ((toneG_L,), (toneG_L,))
     samplesG_L = compute_samples(channelsG_L, 44100 * soundlength_L)
     write_wavefile('tones/G_L.wav', samplesG_L, 44100 * soundlength_L, nchannels=2)
@@ -64,29 +68,29 @@ def precomputeSound():
     samplesA = compute_samples(channelsA, 44100 * soundlength)
     write_wavefile('tones/A.wav', samplesA, 44100 * soundlength, nchannels=2)
 
-    toneA_L   = A(12 * l1, 3 * l2)
+    toneA_L   = A(lengthFactor1 * l1, lengthFactor2 * l2)
     channelsA_L = ((toneA_L,), (toneA_L,))
     samplesA_L = compute_samples(channelsA_L, 44100 * soundlength_L)
     write_wavefile('tones/A_L.wav', samplesA_L, 44100 * soundlength_L, nchannels=2)
 
 
 def C(length1, length2):
-  return islice(damped_wave(frequency=180.0, amplitude=1, length=int(length1/4)), length2)
+  return islice(damped_wave(frequency=180.0, amplitude=1, length=int(length1)), length2)
 
 def D(length1, length2):
-  return islice(damped_wave(frequency=210.0, amplitude=1, length=int(length1/4)), length2)
+  return islice(damped_wave(frequency=210.0, amplitude=1, length=int(length1)), length2)
 
 def E(length1, length2):
-  return islice(damped_wave(frequency=240.0, amplitude=1, length=int(length1/4)), length2)
+  return islice(damped_wave(frequency=240.0, amplitude=1, length=int(length1)), length2)
 
 def F(length1, length2):
-  return islice(damped_wave(frequency=270.0, amplitude=1, length=int(length1/4)), length2)
+  return islice(damped_wave(frequency=270.0, amplitude=1, length=int(length1)), length2)
 
 def G(length1, length2):
-  return islice(damped_wave(frequency=300.0, amplitude=1, length=int(length1/4)), length2)
+  return islice(damped_wave(frequency=300.0, amplitude=1, length=int(length1)), length2)
 
 def A(length1, length2):
-  return islice(damped_wave(frequency=330.0, amplitude=1, length=int(length1/4)), length2)
+  return islice(damped_wave(frequency=330.0, amplitude=1, length=int(length1)), length2)
 
 def violin2(x, y, length):
     l = 44100 * length
